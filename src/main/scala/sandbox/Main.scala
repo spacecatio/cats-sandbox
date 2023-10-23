acea6d975f9b1f865f0188a05cc9fc6c0e598d95
Dave Gurnell <dave@cartographer.io>
2023-10-23 17:27:29 +0100

    Fix typo

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -2,8 +2,8 @@ package sandbox
 
 object Main {
   def greeting(name: String): String =
-    println("Hello " + name + "!")
-  
+    "Hello " + name + "!"
+
   def main(args: Array[String]): Unit =
     println(greeting("world"))
 }
